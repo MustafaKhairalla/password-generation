4458180b3b61045427943840eff065d02c4e1be2
mustafa khairalla <mukhairalla@gmail.com>
2019-12-22 21:04:52 -0700

    html created and javascript done

---
+++ script.js
@@ -0,0 +1,36 @@
+function generatePassword(){
+
+    //setting all charectors
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-";
+    var password = "";
+    
+    //setting the password number of characters 
+    var numberOfChars = document.getElementById("input").value;
+
+    //result value
+    var resultEl = document.getElementById("result");
+
+    //loop to select passwords
+    for(var i=0; i <= numberOfChars; i++){
+        password = password + chars.charAt(Math.floor(Math.random() * Math.floor(chars.length - 1)));
+    }
+
+    //display password to result-area
+    //document.getElementById("result").innerText;
+    resultEl.innerText = password
+
+}
+
+function copyToClipboard(){
+
+    var copyText = document.getElementById("result");
+    copyText.selector()
+    
+    //copyText.select();
+
+    //document.execCommand("copy")
+
+
+
+}
+
